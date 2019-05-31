7c632116f44bf6f80f692e8361a2929b522224f2
gabor <gabor.horovitz@gmail.com>
2019-05-31 09:18:08 +0200

    Add project files.

---
+++ TypeScriptBuildDiagnostics/Scripts/Common/MyBaseClass.ts
@@ -0,0 +1,10 @@
+﻿namespace Common {
+    export class MyBaseClass {
+        model: string;
+
+        protected constructor(model: string) {
+            this.model = model;
+        }
+    }
+}
+
---
+++ TypeScriptBuildDiagnostics/wwwroot/js/gen-ts/MyBaseClass.js
@@ -0,0 +1,10 @@
+var Common;
+(function (Common) {
+    class MyBaseClass {
+        constructor(model) {
+            this.model = model;
+        }
+    }
+    Common.MyBaseClass = MyBaseClass;
+})(Common || (Common = {}));
+//# sourceMappingURL=MyBaseClass.js.map
